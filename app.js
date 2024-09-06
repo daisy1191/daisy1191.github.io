d7dc6aefcea9c46456b0deed6a26e4157bbff7bb
daisy1191 <125830271+daisy1191@users.noreply.github.com>
2024-09-06 14:48:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded" , () => {
+    
+const images= document.querySelectorAll ("img");
+
+for( const image of images) {
+    fetch("https://aws.random.cat/meow").then(response => response.json())
+    .then (data => { 
+     image.src = data.message 
+     image.width=100;
+     image.height=100;
+})
+}
+
+})
